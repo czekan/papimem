8053d47cd3ad3cc836fa38ee126c3b6699e82b81
Tomasz Czekański <t.czekanski@gmail.com>
2017-05-15 22:32:36 +0200

    Create root/CA if not exist. Add usage description to README.md

--- papimem/cli.py
+++ papimem/cli.py
@@ -1,8 +1,14 @@
 import os
+
 import click
+from mitmproxy.certs import CertStore
 
 from papimem.app import PapimemApp
 from papimem.storage.redis import DEFAULT_DSN
+
+
+CONFDIR = "~/.mitmproxy"
+CERTSTORE_BASENAME = "mitmproxy"
 
 
 @click.command()
@@ -18,6 +24,16 @@ from papimem.storage.redis import DEFAULT_DSN
          ' or pass them to remote host and store.')
 def run(proxy_port, storage_dsn, mock_mode):
     """ Python API request-response memorizer """
+    # make sure root CA is generated for mitmproxy library
+    ca_path = os.path.expanduser(
+        os.path.join(CONFDIR, CERTSTORE_BASENAME, "-ca.pem")
+    )
+    if not os.path.exists(ca_path):
+        CertStore.from_store(
+            os.path.expanduser(CONFDIR),
+            CERTSTORE_BASENAME
+        )
+
     app = PapimemApp(proxy_port, storage_dsn, mock_mode)
     app.run()
 
